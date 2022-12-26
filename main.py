8683e2a33e6d38d18e7eead2de86a5a60ef192bf
Sam Poad <sam.poad@gmail.com>
2022-12-25 21:32:56 -0500

    fixed table_preparation needing database changes to be committed, and added checks to main.py

--- main.py
+++ main.py
@@ -3,8 +3,24 @@
 import sqlite3 
 from table_preparation import prepare_table
 
-permanent_blocks = cursor.execute(
-    "SELECT item FROM known_fraud_item WHERE expiry_date IS NULL"
-    ).fetchall()
+# CREATE THE CONNECTION TO THE DATABASE 
+connection = sqlite3.connect("monitor.db")
 
-print(permanent_blocks)
+# CREATE THE CURSOR 
+cursor = connection.cursor() 
+
+print("Connected to the Monitor Database")
+
+permanent_blocks = cursor.execute(
+    "SELECT * FROM known_fraud_item WHERE expiry_date IS NULL").fetchall()
+print("Showing permanently blocked email addresses")
+print([record for record in permanent_blocks])
+
+temporary_blocks = cursor.execute(
+    """SELECT * FROM known_fraud_item WHERE expiry_date IS NOT NULL
+    """).fetchall()
+print("Showing temporarily blocked emails addresses")
+print([_ for _ in temporary_blocks])
+
+# for row in cursor.execute("SELECT * FROM known_fraud_item"): 
+#    print(row)
--- table_preparation.py
+++ table_preparation.py
@@ -11,7 +11,7 @@ def prepare_table():
 
     # CREATE THE TABLE IF IT DOES NOT EXIST 
     try: 
-        cursor.execute("SELECT id FROM known_fraud_item ORDER BY last_update DESC LIMIT 1")
+        print(cursor.execute("SELECT item FROM known_fraud_item ORDER BY last_update DESC LIMIT 1").fetchone())
         print("Table: known_fraud_item already exists") 
         # in which case we should drop the table contents
         # cursor.execute("DROP TABLE known_fraud_item")
@@ -35,28 +35,29 @@ def prepare_table():
     # FIRST BATCH ARE THOSE THAT ARE WITHOUT AN EXPIRY_DATE 
     cursor.execute(
         """
-        INSERT INTO known_fraud_item (item, item_type, create_date) VALUES (
-            'bruce.wayne@w_enterprises.com', 'email', CURRENT_TIMESTAMP), (
-            'batman@batcave.com', 'email', CURRENT_TIMESTAMP), (
-            'ckent@dailyplanet.com', 'email', CURRENT_TIMESTAMP)
+        INSERT INTO known_fraud_item (id, item, item_type, create_date) VALUES (
+            0, 'bruce.wayne@w_enterprises.com', 'email', CURRENT_TIMESTAMP), (
+            1, 'batman@batcave.com', 'email', CURRENT_TIMESTAMP), (
+            2, 'ckent@dailyplanet.com', 'email', CURRENT_TIMESTAMP)
         """
     )
 
     # SECOND BATCH HAVE AN EXPIRATION DATE
     cursor.execute(
         """
-        INSERT INTO known_fraud_item (item, item_type, create_date, expiry_date) VALUES (
-            'tony@starkindustries.com', 'email', CURRENT_TIMESTAMP, '2022-12-25 00:00:00'), (
-            'steverogers@whitehouse.gov', 'email', CURRENT_TIMESTAMP, '2022-12-25 00:00:00'), (
-            'pparker@dailybugle.com', 'email', CURRENT_TIMESTAMP, '2022-12-25 00:00:00')
+        INSERT INTO known_fraud_item (id, item, item_type, create_date, expiry_date) VALUES (
+            3, 'tony@starkindustries.com', 'email', CURRENT_TIMESTAMP, '2022-12-25 00:00:00'), (
+            4, 'steverogers@whitehouse.gov', 'email', CURRENT_TIMESTAMP, '2022-12-25 00:00:00'), (
+            5, 'pparker@dailybugle.com', 'email', CURRENT_TIMESTAMP, '2022-12-25 00:00:00')
         """
     )
 
+    connection.commit()
     # VERIFY THE DATA WAS INSERTED
     # verification = cursor.execute('SELECT * FROM known_fraud_item')
     # print(f"{cursor.execute('SELECT * FROM known_fraud_item LIMIT 6')}")
     # print(verification.fetchall())
-    print(cursor.execute('SELECT * FROM known_fraud_item LIMIT 6').fetchall())
+    # print(cursor.execute('SELECT * FROM known_fraud_item LIMIT 6').fetchall())
 
 # CALL THE FUNCTION 
 prepare_table()
